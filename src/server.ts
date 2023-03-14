b5e0175c39d17fe98bba3b96c065263fa3603472
IgorAlves <igor.alves@whitespace.ch>
2023-03-14 17:15:03 +0000

    Secure the application against some attacks

--- src/server.ts
+++ src/server.ts
@@ -2,13 +2,30 @@ import express from "express";
 import router from "./routes";
 import dotenv from "dotenv";
 import { PrismaClient } from "@prisma/client";
+import helmet from "helmet";
+// @ts-ignore
+import xssClean from "xss-clean";
+// @ts-ignore
+import hpp from "hpp";
+import mongoSanitize from "express-mongo-sanitize";
+import rateLimit from "express-rate-limit";
 
 const prisma = new PrismaClient();
+
+const limiter = rateLimit({
+  windowMs: 10 * 60 * 100,
+  max: 100,
+});
 
 const app = express();
 dotenv.config();
 
 app.use(express.json());
+app.use(helmet());
+app.use(xssClean());
+app.use(hpp());
+app.use(mongoSanitize());
+app.use(limiter);
 
 app.use("/api", router);
 
